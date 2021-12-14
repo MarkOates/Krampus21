6f6478d61cd28a1377ee367668f83267336ab1cd
Mark Oates <markoates0@gmail.com>
2021-12-13 23:20:38 -0500

    * Add test for inherited/assigned type

--- tests/Krampus21/DialogBoxes/BaseTest.cpp
+++ tests/Krampus21/DialogBoxes/BaseTest.cpp
@@ -3,8 +3,22 @@
 
 #include <Krampus21/DialogBoxes/Base.hpp>
 
+class MyTestDialogBox : public Krampus21::DialogBoxes::Base
+{
+public:
+   MyTestDialogBox() : Krampus21::DialogBoxes::Base("MyTestDialogBox") {}
+};
+
+
 TEST(Krampus21_DialogBoxes_BaseTest, can_be_created_without_blowing_up)
 {
    Krampus21::DialogBoxes::Base base;
+}
+
+TEST(Krampus21_DialogBoxes_BaseTest, when_derived_the_type_of_the_derived_class_is_set)
+{
+   std::string expected_type = "MyTestDialogBox";
+   MyTestDialogBox dialog_box;
+   ASSERT_EQ(expected_type, dialog_box.get_type());
 }
 
