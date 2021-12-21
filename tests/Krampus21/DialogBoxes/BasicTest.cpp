7dbf3f28f348570c920968171cfdb8863efbe038
Mark Oates <markoates0@gmail.com>
2021-12-20 19:24:11 -0500

    * Add tests for #get_type# on derived classes for DialogBoxes/*

--- tests/Krampus21/DialogBoxes/BasicTest.cpp
+++ tests/Krampus21/DialogBoxes/BasicTest.cpp
@@ -8,3 +8,9 @@ TEST(Krampus21_DialogBoxes_BasicTest, can_be_created_without_blowing_up)
    Krampus21::DialogBoxes::Basic basic;
 }
 
+TEST(Krampus21_DialogBoxes_BasicTest, get_type__returns_the_expected_value)
+{
+   Krampus21::DialogBoxes::Basic basic;
+   EXPECT_EQ("Basic", basic.get_type());
+}
+
--- tests/Krampus21/DialogBoxes/ChoiceTest.cpp
+++ tests/Krampus21/DialogBoxes/ChoiceTest.cpp
@@ -8,3 +8,9 @@ TEST(Krampus21_DialogBoxes_ChoiceTest, can_be_created_without_blowing_up)
    Krampus21::DialogBoxes::Choice choice;
 }
 
+TEST(Krampus21_DialogBoxes_ChoiceTest, get_type__returns_the_expected_value)
+{
+   Krampus21::DialogBoxes::Choice choice;
+   EXPECT_EQ("Choice", choice.get_type());
+}
+
