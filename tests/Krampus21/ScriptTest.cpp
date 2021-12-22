b04d3899ff5bb8d70339ed045d54c1434ceb2686
Mark Oates <markoates0@gmail.com>
2021-12-21 21:09:56 -0500

    * Add some nice cosmetic blank lines in ScriptTest

--- tests/Krampus21/ScriptTest.cpp
+++ tests/Krampus21/ScriptTest.cpp
@@ -3,50 +3,60 @@
 
 #include <Krampus21/Script.hpp>
 
+
 TEST(Krampus21_ScriptTest, can_be_created_without_blowing_up)
 {
    Krampus21::Script script;
 }
+
 
 TEST(Krampus21_ScriptText, get_current_line_text__gets_the_text_from_the_current_line)
 {
    // TODO
 }
 
+
 TEST(Krampus21_ScriptText, at_last_line__returns_true_if_on_the_last_line)
 {
    // TODO
 }
+
 
 TEST(Krampus21_ScriptText, at_last_line__returns_false_if_not_on_the_last_line)
 {
    // TODO
 }
 
+
 TEST(Krampus21_ScriptText, goto_next_line__with_no_lines_returns_false)
 {
    // TODO
 }
+
 
 TEST(Krampus21_ScriptText, goto_next_line__will_advance_the_current_line_num)
 {
    // TODO
 }
 
+
 TEST(Krampus21_ScriptText, goto_next_line__if_currently_at_the_last_line__returns_false)
 {
    // TODO
 }
+
 
 TEST(Krampus21_ScriptText, goto_next_line__if_currently_at_the_last_line__sets_the_current_line_num_to_negative_1)
 {
    // TODO
 }
 
+
 TEST(Krampus21_ScriptText, goto_next_line__if_currently_at_the_last_line__sets_the_finished_to_true)
 {
    // TODO
 }
+
 
 TEST(Krampus21_ScriptText, goto_line_num__sets_the_current_line_num__and_returns_true)
 {
@@ -61,6 +71,7 @@ TEST(Krampus21_ScriptText, goto_line_num__sets_the_current_line_num__and_returns
    ASSERT_EQ(true, script.goto_line_num(2));
    ASSERT_EQ(2, script.get_current_line_num());
 }
+
 
 TEST(Krampus21_ScriptText, goto_line_num__works_for_the_whole_range_of_valid_line_numbers)
 {
@@ -80,6 +91,7 @@ TEST(Krampus21_ScriptText, goto_line_num__works_for_the_whole_range_of_valid_lin
    }
 }
 
+
 TEST(Krampus21_ScriptText, goto_line_num__if_outside_the_range_of_lines__and_returns_false)
 {
    std::vector<std::string> script_lines = {
@@ -96,4 +108,5 @@ TEST(Krampus21_ScriptText, goto_line_num__if_outside_the_range_of_lines__and_ret
    ASSERT_EQ(false, script.goto_line_num(4));
    ASSERT_EQ(false, script.goto_line_num(999));
 }
+
 
