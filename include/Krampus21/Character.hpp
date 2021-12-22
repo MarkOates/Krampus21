8a7ae812524d1a0c00eb474ef7700408c8be6f5d
Mark Oates <markoates0@gmail.com>
2021-12-22 13:48:55 -0500

    * Add Krampus21/Character

---
+++ include/Krampus21/Character.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+
+#include <string>
+
+
+namespace Krampus21
+{
+   class Character
+   {
+   private:
+
+   public:
+      Character();
+      ~Character();
+
+      std::string run();
+   };
+}
+
+
+
---
+++ src/Krampus21/Character.cpp
@@ -0,0 +1,27 @@
+
+
+#include <Krampus21/Character.hpp>
+
+
+
+namespace Krampus21
+{
+
+
+Character::Character()
+{
+}
+
+
+Character::~Character()
+{
+}
+
+
+std::string Character::run()
+{
+   return "Hello World!";
+}
+} // namespace Krampus21
+
+
