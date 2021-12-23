64a0f9c678af4f701c8a61eaa8248915ffe2922b
Mark Oates <markoates0@gmail.com>
2021-12-23 16:16:24 -0500

    Tweak camera zoom scale presets for characters; Tweak script

--- src/Krampus21/Character.cpp
+++ src/Krampus21/Character.cpp
@@ -80,8 +80,8 @@ void Character::frame_wide()
 
 void Character::frame_closeup()
 {
-   place.scale.x = 1.57;
-   place.scale.y = 1.57;
+   place.scale.x = 1.8;
+   place.scale.y = 1.8;
    return;
 }
 
