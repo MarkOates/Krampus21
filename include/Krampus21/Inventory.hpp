d3157519e3878a8a15f9249b222fb5b5b91dd100
Mark Oates <markoates0@gmail.com>
2021-12-24 01:26:09 -0500

    * Add some more elements necessary for rendering inventory

--- include/Krampus21/Inventory.hpp
+++ include/Krampus21/Inventory.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
 
+#include <AllegroFlare/BitmapBin.hpp>
+#include <AllegroFlare/FontBin.hpp>
 #include <AllegroFlare/Inventory.hpp>
+#include <allegro_flare/placement2d.h>
 
 
 namespace Krampus21
@@ -9,12 +12,18 @@ namespace Krampus21
    class Inventory
    {
    private:
+      AllegroFlare::FontBin* font_bin;
+      AllegroFlare::BitmapBin* bitmap_bin;
       AllegroFlare::Inventory* af_inventory;
+      allegro_flare::placement2d place;
 
    public:
-      Inventory();
+      Inventory(AllegroFlare::FontBin* font_bin=nullptr, AllegroFlare::BitmapBin* bitmap_bin=nullptr);
       ~Inventory();
 
+      void set_font_bin(AllegroFlare::FontBin* font_bin);
+      void set_bitmap_bin(AllegroFlare::BitmapBin* bitmap_bin);
+      allegro_flare::placement2d get_place();
       void render();
    };
 }
--- src/Krampus21/Inventory.cpp
+++ src/Krampus21/Inventory.cpp
@@ -9,14 +9,35 @@ namespace Krampus21
 {
 
 
-Inventory::Inventory()
-   : af_inventory(nullptr)
+Inventory::Inventory(AllegroFlare::FontBin* font_bin, AllegroFlare::BitmapBin* bitmap_bin)
+   : font_bin(font_bin)
+   , bitmap_bin(bitmap_bin)
+   , af_inventory(nullptr)
+   , place({ 1920/2, 1080/5*2, 800, 800 })
 {
 }
 
 
 Inventory::~Inventory()
 {
+}
+
+
+void Inventory::set_font_bin(AllegroFlare::FontBin* font_bin)
+{
+   this->font_bin = font_bin;
+}
+
+
+void Inventory::set_bitmap_bin(AllegroFlare::BitmapBin* bitmap_bin)
+{
+   this->bitmap_bin = bitmap_bin;
+}
+
+
+allegro_flare::placement2d Inventory::get_place()
+{
+   return place;
 }
 
 
