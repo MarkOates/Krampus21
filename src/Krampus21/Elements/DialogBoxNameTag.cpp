1a69213200585acd1b5e1dda80fdbee30130318b
Mark Oates <markoates0@gmail.com>
2021-12-23 23:50:03 -0500

    * Use DialogBoxFrame in DialogBoxNameTag

--- src/Krampus21/Elements/DialogBoxNameTag.cpp
+++ src/Krampus21/Elements/DialogBoxNameTag.cpp
@@ -5,6 +5,7 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_color.h>
+#include <Krampus21/Elements/DialogBoxFrame.hpp>
 #include <stdexcept>
 #include <sstream>
 #include <allegro5/allegro_color.h>
@@ -70,38 +71,8 @@ void DialogBoxNameTag::render()
          error_message << "DialogBoxNameTag" << "::" << "render" << ": error: " << "guard \"al_get_current_display()\" not met";
          throw std::runtime_error(error_message.str());
       }
-   float roundness = 18.0f;
-   float border_thickness = 5.0f;
-   float border_inner_padding = border_thickness * 3;
-   //ALLEGRO_COLOR fill_color = al_color_html("162428");
-   float opacity = 0.7;
-   ALLEGRO_COLOR fill_color = ALLEGRO_COLOR{0.02f*opacity, 0.0f*opacity, 0.1f*opacity, opacity};
-   ALLEGRO_COLOR border_color = al_color_html("244751");
-
-   // draw backfill
-   al_draw_filled_rounded_rectangle(
-      0 + border_inner_padding,
-      0 + border_inner_padding,
-      width - border_inner_padding,
-      height - border_inner_padding,
-      roundness * 0.5,
-      roundness * 0.5,
-      fill_color
-   );
-   // draw border
-   al_draw_rounded_rectangle(
-      0,
-      0,
-      width,
-      height,
-      roundness,
-      roundness,
-      border_color,
-      border_thickness
-   );
-
+   Krampus21::Elements::DialogBoxFrame(width, height).render();
    draw_text();
-
    return;
 }
 
