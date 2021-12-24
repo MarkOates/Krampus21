54b7216758df5fad54357fd09c15207dc54a521c
Mark Oates <markoates0@gmail.com>
2021-12-24 01:36:54 -0500

    * Add scaffold draft for Krampus21/Inventory

--- include/Krampus21/Inventory.hpp
+++ include/Krampus21/Inventory.hpp
@@ -16,6 +16,13 @@ namespace Krampus21
       AllegroFlare::BitmapBin* bitmap_bin;
       AllegroFlare::Inventory* af_inventory;
       allegro_flare::placement2d place;
+      int cursor_x;
+      int cursor_y;
+      int num_columns;
+      int num_rows;
+      bool active;
+      float active_animation_counter;
+      float focus_selection_animation_counter;
 
    public:
       Inventory(AllegroFlare::FontBin* font_bin=nullptr, AllegroFlare::BitmapBin* bitmap_bin=nullptr);
@@ -24,7 +31,18 @@ namespace Krampus21
       void set_font_bin(AllegroFlare::FontBin* font_bin);
       void set_bitmap_bin(AllegroFlare::BitmapBin* bitmap_bin);
       allegro_flare::placement2d get_place();
+      void update();
       void render();
+      void draw_backframe();
+      void draw_inventory_title_text();
+      void draw_inventory_boxes_and_elevated_item_selection();
+      void draw_details_frame();
+      void move_cursor_up();
+      void move_cursor_down();
+      void move_cursor_left();
+      void move_cursor_right();
+      void activate();
+      void deactivate();
    };
 }
 
--- src/Krampus21/Inventory.cpp
+++ src/Krampus21/Inventory.cpp
@@ -14,6 +14,13 @@ Inventory::Inventory(AllegroFlare::FontBin* font_bin, AllegroFlare::BitmapBin* b
    , bitmap_bin(bitmap_bin)
    , af_inventory(nullptr)
    , place({ 1920/2, 1080/5*2, 800, 800 })
+   , cursor_x(0)
+   , cursor_y(0)
+   , num_columns(4)
+   , num_rows(3)
+   , active(false)
+   , active_animation_counter(0)
+   , focus_selection_animation_counter(0)
 {
 }
 
@@ -41,6 +48,12 @@ allegro_flare::placement2d Inventory::get_place()
 }
 
 
+void Inventory::update()
+{
+   // TODO
+   return;
+}
+
 void Inventory::render()
 {
    if (!(af_inventory))
@@ -49,6 +62,70 @@ void Inventory::render()
          error_message << "Inventory" << "::" << "render" << ": error: " << "guard \"af_inventory\" not met";
          throw std::runtime_error(error_message.str());
       }
+   draw_backframe();
+   draw_inventory_title_text();
+   draw_inventory_boxes_and_elevated_item_selection();
+   draw_details_frame();
+   return;
+}
+
+void Inventory::draw_backframe()
+{
+   // TODO
+   return;
+}
+
+void Inventory::draw_inventory_title_text()
+{
+   // TODO
+   return;
+}
+
+void Inventory::draw_inventory_boxes_and_elevated_item_selection()
+{
+   // TODO
+   return;
+}
+
+void Inventory::draw_details_frame()
+{
+   // TODO
+   return;
+}
+
+void Inventory::move_cursor_up()
+{
+   // TODO
+   return;
+}
+
+void Inventory::move_cursor_down()
+{
+   // TODO
+   return;
+}
+
+void Inventory::move_cursor_left()
+{
+   // TODO
+   return;
+}
+
+void Inventory::move_cursor_right()
+{
+   // TODO
+   return;
+}
+
+void Inventory::activate()
+{
+   // TODO
+   return;
+}
+
+void Inventory::deactivate()
+{
+   // TODO
    return;
 }
 } // namespace Krampus21
