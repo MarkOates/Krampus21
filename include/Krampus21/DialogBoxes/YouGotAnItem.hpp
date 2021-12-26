8f30bdcc6605a6f5e21097a3a202aee290968959
Mark Oates <markoates0@gmail.com>
2021-12-25 21:43:04 -0500

    * Add #item_num# propoerty to DialogBoxes/YouGotAnItem

---
+++ include/Krampus21/DialogBoxes/YouGotAnItem.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include <Krampus21/DialogBoxes/Base.hpp>
+
+
+namespace Krampus21
+{
+   namespace DialogBoxes
+   {
+      class YouGotAnItem : public Krampus21::DialogBoxes::Base
+      {
+      private:
+         int item_num;
+
+      public:
+         YouGotAnItem(int item_num=0);
+         ~YouGotAnItem();
+
+      };
+   }
+}
+
+
+
---
+++ src/Krampus21/DialogBoxes/YouGotAnItem.cpp
@@ -0,0 +1,28 @@
+
+
+#include <Krampus21/DialogBoxes/YouGotAnItem.hpp>
+
+
+
+namespace Krampus21
+{
+namespace DialogBoxes
+{
+
+
+YouGotAnItem::YouGotAnItem(int item_num)
+   : Krampus21::DialogBoxes::Base("YouGotAnItem")
+   , item_num(item_num)
+{
+}
+
+
+YouGotAnItem::~YouGotAnItem()
+{
+}
+
+
+} // namespace DialogBoxes
+} // namespace Krampus21
+
+
