ff35c418f6a8fd36c81853979fa11bf784fef5ef
Mark Oates <markoates0@gmail.com>
2021-12-25 21:59:29 -0500

    * Add rendering fixture to YouGotAnItemRenderer

--- tests/Krampus21/DialogBoxRenderers/YouGotAnItemRendererTest.cpp
+++ tests/Krampus21/DialogBoxRenderers/YouGotAnItemRendererTest.cpp
@@ -1,7 +1,20 @@
 
 #include <gtest/gtest.h>
 
+#define ASSERT_THROW_WITH_MESSAGE(code, raised_exception_type, raised_exception_message) \
+   try { code; FAIL() << "Expected " # raised_exception_type; } \
+   catch ( raised_exception_type const &err ) { EXPECT_EQ(err.what(), std::string( raised_exception_message )); } \
+   catch (...) { FAIL() << "Expected " # raised_exception_type; }
+
+
 #include <Krampus21/DialogBoxRenderers/YouGotAnItemRenderer.hpp>
+
+#include <AllegroFlare/Testing/WithAllegroRenderingFixture.hpp>
+
+class Krampus21_DialogBoxRenderers_YouGotAnItemRendererTest : public ::testing::Test {};
+class Krampus21_DialogBoxRenderers_YouGotAnItemRendererWithAllegroRenderingFixtureTest
+   : public AllegroFlare::Testing::WithAllegroRenderingFixture {};
+
 
 TEST(Krampus21_DialogBoxRenderers_YouGotAnItemRendererTest, can_be_created_without_blowing_up)
 {
