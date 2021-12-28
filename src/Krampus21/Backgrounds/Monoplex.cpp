5da3d0c4fdd2b94fef74478cf2c0423510b1ae3b
Mark Oates <markoates0@gmail.com>
2021-12-28 18:59:26 -0500

    * Add placement and render monoplex graphics

--- src/Krampus21/Backgrounds/Monoplex.cpp
+++ src/Krampus21/Backgrounds/Monoplex.cpp
@@ -1,6 +1,7 @@
 
 
 #include <Krampus21/Backgrounds/Monoplex.hpp>
+#include <allegro_flare/placement2d.h>
 #include <stdexcept>
 #include <sstream>
 
@@ -31,9 +32,26 @@ void Monoplex::draw()
          error_message << "Monoplex" << "::" << "draw" << ": error: " << "guard \"bitmap_bin\" not met";
          throw std::runtime_error(error_message.str());
       }
+   float time_now = al_get_time();
    ALLEGRO_BITMAP *monoplex_1_bitmap = bitmap_bin->auto_get("monoplex-1.png");
    ALLEGRO_BITMAP *monoplex_2_bitmap = bitmap_bin->auto_get("monoplex-2.png");
    ALLEGRO_BITMAP *monoplex_3_bitmap = bitmap_bin->auto_get("monoplex-3.png");
+   allegro_flare::placement2d monoplex_1_place;
+   allegro_flare::placement2d monoplex_2_place;
+   allegro_flare::placement2d monoplex_3_place;
+
+   monoplex_1_place.start_transform();
+   al_draw_bitmap(monoplex_1_bitmap, 0, 0, 0);
+   monoplex_1_place.restore_transform();
+
+   monoplex_2_place.start_transform();
+   al_draw_bitmap(monoplex_2_bitmap, 0, 0, 0);
+   monoplex_2_place.restore_transform();
+
+   monoplex_3_place.start_transform();
+   al_draw_bitmap(monoplex_3_bitmap, 0, 0, 0);
+   monoplex_3_place.restore_transform();
+
    return;
 }
 } // namespace Backgrounds
