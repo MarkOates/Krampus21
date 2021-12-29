f7c29b6076c26a281f6f7844fdc6081a3f376cb2
Mark Oates <markoates0@gmail.com>
2021-12-28 23:26:52 -0500

    * Add rendering logic (though not tested) for Backgrounds/Image

--- include/Krampus21/Backgrounds/Image.hpp
+++ include/Krampus21/Backgrounds/Image.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 
+#include <AllegroFlare/BitmapBin.hpp>
+#include <Krampus21/Backgrounds/Base.hpp>
+#include <allegro5/allegro.h>
 #include <string>
 
 
@@ -8,15 +11,18 @@ namespace Krampus21
 {
    namespace Backgrounds
    {
-      class Image
+      class Image : public Krampus21::Backgrounds::Base
       {
       private:
+         AllegroFlare::BitmapBin* bitmap_bin;
+         std::string image_filename;
 
       public:
-         Image();
+         Image(AllegroFlare::BitmapBin* bitmap_bin=nullptr);
          ~Image();
 
-         std::string run();
+         void draw() override;
+         ALLEGRO_BITMAP* obtain_background_bitmap();
       };
    }
 }
--- src/Krampus21/Backgrounds/Image.cpp
+++ src/Krampus21/Backgrounds/Image.cpp
@@ -1,7 +1,9 @@
 
 
 #include <Krampus21/Backgrounds/Image.hpp>
-
+#include <allegro_flare/placement2d.h>
+#include <stdexcept>
+#include <sstream>
 
 
 namespace Krampus21
@@ -10,7 +12,10 @@ namespace Backgrounds
 {
 
 
-Image::Image()
+Image::Image(AllegroFlare::BitmapBin* bitmap_bin)
+   : Krampus21::Backgrounds::Base("Monoplex")
+   , bitmap_bin(bitmap_bin)
+   , image_filename("[image-filename-not-set]")
 {
 }
 
@@ -20,9 +25,25 @@ Image::~Image()
 }
 
 
-std::string Image::run()
+void Image::draw()
 {
-   return "Hello World!";
+   if (!(bitmap_bin))
+      {
+         std::stringstream error_message;
+         error_message << "Image" << "::" << "draw" << ": error: " << "guard \"bitmap_bin\" not met";
+         throw std::runtime_error(error_message.str());
+      }
+   allegro_flare::placement2d background_image_placement;
+   ALLEGRO_BITMAP *bitmap = obtain_background_bitmap();
+   background_image_placement.start_transform();
+   al_draw_bitmap(bitmap, 0, 0, 0);
+   background_image_placement.restore_transform();
+   return;
+}
+
+ALLEGRO_BITMAP* Image::obtain_background_bitmap()
+{
+   return bitmap_bin->auto_get(image_filename);
 }
 } // namespace Backgrounds
 } // namespace Krampus21
