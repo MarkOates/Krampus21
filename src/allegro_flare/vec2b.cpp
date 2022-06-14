1d30c2aea2f9d796759a442732ef364862b5654c
Mark Oates <markoates0@gmail.com>
2022-06-14 18:27:49 -0400

    Replace allegro_flare/placement2d,vec2b that were symlinks with hardcoded files

--- src/allegro_flare/vec2b.cpp
+++ src/allegro_flare/vec2b.cpp
@@ -1,1 +1,18 @@
-../../../allegro_flare/src/allegro_flare/vec2b.cpp
+
+
+
+#include <allegro_flare/vec2b.h>
+
+
+
+namespace allegro_flare
+{
+   vec2b::vec2b(bool x, bool y)
+      : x(x)
+      , y(y)
+   {
+   }
+}
+
+
+
